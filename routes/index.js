938ea83f5181386ac6caa2cdb5464d95d4460ac4
quinali <quinali@gmail.com>
2015-08-17 13:09:57 +0000

    Https activado

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -26,15 +27,16 @@ router.get('/quizes?search=(\\a+)', quizController.search);
 router.get('/quizes',                         quizController.index);
 router.get('/quizes/:quizId(\\d+)',           quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',    quizController.answer);
-router.get('/quizes/new',		   		      quizController.new	);
-router.post('/quizes/create',		          quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',      quizController.edit);
-router.put('/quizes/:quizId(\\d+)',           quizController.update);
-router.delete('/quizes/:quizId(\\d+)',        quizController.destroy );
+router.get('/quizes/new',		      sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',		      sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',      sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',           sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 //Por ejercicio modulo 6
 router.get('/author', function(req, res) {
